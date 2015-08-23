1c14e0c3dd087b420ac33d01d526dd5966828095
Daniel Booth <daniel@nevermiss.com.au>
2015-08-23 18:04:42 +1000

    Commiting from Mac to move over to Windows

--- Getting and Cleaning Data/course_project/run_analysis.R
+++ Getting and Cleaning Data/course_project/run_analysis.R
@@ -1,10 +1,13 @@
 # == Getting and Cleaning Data - Course Project - Tidying Recipe == #
 
-# Download the file
-setInternet2(TRUE) # Resolves some PCs' issues with https download.file
+# Download the zip file
+setInternet2(TRUE) # Resolves some Windows PCs' issues with https download.file
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-setwd("~/GitHub/datasciencecoursera/Getting and Cleaning Data")
-download.file(fileURL, destfile = "data/UCI_HAR.zip")
+# Have to take off the https for mac
+fileURL_mac <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+setwd("~/GitHub/datasciencecoursera/Getting and Cleaning Data") # Windows
+setwd("~/RStudio/datasciencecoursera/Getting and Cleaning Data/course_project/") # Mac
+download.file(fileURL_mac, destfile = "data_in/UCI_HAR.zip")
 
 # Unzip
 unzip("data/UCI_HAR.zip",exdir = "data")
